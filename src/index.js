80a521c00c90f316bfe33deeb10dc4aac987793f
weineko <gulingxi@glxycl.top>
2024-06-24 10:35:18 +0800

    更改 src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import DOCS from './help.html'
- 
-
 
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
@@ -155,14 +152,4 @@ async function fetchToken(wwwAuthenticate, scope, authorization) {
     headers.set("Authorization", authorization);
   }
   return await fetch(url, { method: "GET", headers: headers });
-}
-
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
 }
