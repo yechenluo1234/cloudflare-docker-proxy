7e687013cdee25b52ad38e791a170d3c3eaa8973
GraiceWei <51827381+yechenluo1234@users.noreply.github.com>
2024-06-24 09:55:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.waw.gay": dockerHub,
+  "quay.waw.gay": "https://quay.io",
+  "gcr.waw.gay": "https://gcr.io",
+  "k8s-gcr.waw.gay": "https://k8s.gcr.io",
+  "k8s.waw.gay": "https://registry.k8s.io",
+  "ghcr.waw.gay": "https://ghcr.io",
+  "cloudsmith.waw.gay": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.waw.gay": dockerHub,
 };
 
 function routeByHosts(host) {
